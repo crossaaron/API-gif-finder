80b1d50d30ae3a7eb82f16d5355d6f1a75d51763
Aaron Cross <aaroncross@Aarons-MBP.fios-router.home>
2017-12-03 09:55:10 -0500

    gif click start/stop functionality working

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,25 +2,45 @@ $("button").on("click", function() {
       var person = $(this).attr("data-person");
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         person + "&api_key=dc6zaTOxFJmzC&limit=10";
-      console.log(this);
+      // calling ajax to get api//
       $.ajax({
           url: queryURL,
           method: "GET"
         })
         .done(function(response) {
           var results = response.data;
-  console
+    // looping through results to generate divs with images
           for (var i = 0; i < results.length; i++) {
             var gifDiv = $("<div class='item'>");
-
             var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height_still.url);
-
             
+    //assigning data to image
+            personImage.attr("src", results[i].images.fixed_height_still.url);
+            personImage.attr("data-still", results[i].images.fixed_height_still.url);
+            personImage.attr("data-state", "still");
+            personImage.attr("data-animate", results[i].images.fixed_height.url)
+            personImage.attr("id", "randGif");
+ 
+    //creating a DOM div element
             gifDiv.prepend(personImage);
-
+    //putting image into div
             $("#gifs-appear-here").prepend(gifDiv);
+    // listener for gif click to start/stop/restart gif
+            $("#randGif").on('click', function () {
+
+              var state = $(this).attr("data-state");
+              
+              if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+              } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+              };
+
+            });
           }
+
 
         });
     });
