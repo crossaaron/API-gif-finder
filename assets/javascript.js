7ca524b34aa7143f7e3f780c7a93314374cafa1e
Aaron Cross <aaroncross@Aarons-MBP.fios-router.home>
2017-12-05 23:24:16 -0500

    still buggy but mostly complete

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,61 +1,117 @@
 $("button").on("click", function() {
-      var person = $(this).attr("data-person");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=dc6zaTOxFJmzC&limit=10";
+  var gifSelection = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSelection + "&api_key=dc6zaTOxFJmzC&limit=10";
       // calling ajax to get api//
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        .done(function(response) {
-          var results = response.data;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).done(function(response) {
+      var results = response.data;
     // looping through results to generate divs with images
-          for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='item'>");
-            var personImage = $("<img>");
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class='item'>");
+        var gifImage = $("<img>");
             
     //assigning data to image
-            personImage.attr("src", results[i].images.fixed_height_still.url);
-            personImage.attr("data-still", results[i].images.fixed_height_still.url);
-            personImage.attr("data-state", "still");
-            personImage.attr("data-animate", results[i].images.fixed_height.url)
-            personImage.attr("id", "randGif");
+        gifImage.attr("src", results[i].images.fixed_height_still.url);
+        gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+        gifImage.attr("data-state", "still");
+        gifImage.attr("data-animate", results[i].images.fixed_height.url)
+        gifImage.attr("id", "randGif");
  
     //creating a DOM div element
-            gifDiv.prepend(personImage);
+        gifDiv.prepend(gifImage);
     //putting image into div
-            $("#gifs-appear-here").prepend(gifDiv);
+        $("#gifs-appear-here").prepend(gifDiv);
+    
     // listener for gif click to start/stop/restart gif
-            $("#randGif").on('click', function () {
+        $("#randGif").on('click', function () {
 
-              var state = $(this).attr("data-state");
+          var state = $(this).attr("data-state");
               
-              if (state === "still") {
-                $(this).attr("src", $(this).attr("data-animate"));
-                $(this).attr("data-state", "animate");
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
               } else {
                 $(this).attr("src", $(this).attr("data-still"));
                 $(this).attr("data-state", "still");
-              };
-
-            });
-          }
-
+                };
 
         });
+      }
+      
     });
+});
 
-$("#add-movie").on("click", function(event) {
-        event.preventDefault();
-        // This line of code will grab the input from the textbox
-        var movie = $("#movie-input").val().trim();
-
-        // The movie from the textbox is then added to our array
+$("#add-gif").on("click", function(event) {
+  event.preventDefault();
         
-        movies.push(movie);
+  renderButtons();
+});
 
-        // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
-      });
+
+
+function renderButtons() {
+  
+  var gifSelection = $("#gif-input").val().trim();
+  var newGifButton = $("<button>");
+  newGifButton.attr("data-name", gifSelection);
+  newGifButton.text(gifSelection);
+
+  $("#buttons").append(newGifButton);
+
+  $("#buttons").on("click", function() {
+
+  
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSelection + "&api_key=dc6zaTOxFJmzC&limit=10";
+      // calling ajax to get api//
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).done(function(response) {
+      var results = response.data;
+    // looping through results to generate divs with images
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class='item'>");
+        var gifImage = $("<img>");
+            
+    //assigning data to image
+        gifImage.attr("src", results[i].images.fixed_height_still.url);
+        gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+        gifImage.attr("data-state", "still");
+        gifImage.attr("data-animate", results[i].images.fixed_height.url)
+        gifImage.attr("id", "randGif");
+ 
+
+
+    //creating a DOM div element
+        gifDiv.prepend(gifImage);
+    //putting image into div
+        $("#gifs-appear-here").prepend(gifDiv);
+    
+    // listener for gif click to start/stop/restart gif
+        $("#randGif").on('click', function () {
+
+          var state = $(this).attr("data-state");
+              
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+              } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+                };
+
+        });
+      }
+      
+    });
+  });
+};
+  
+
+
+
+
 
 
